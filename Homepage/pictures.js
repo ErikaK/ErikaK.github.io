1432971d61108144c8ce7c9892095ef2ee8c1771
neha45556 <neha45556@gmail.com>
2015-07-24 03:44:47 -0700

    pictures for loop
    picture js

--- Homepage/pictures.js
+++ Homepage/pictures.js
@@ -1,6 +1,12 @@
 var picture = ["cal.jpg","flower.jpg","god.jpg","palmtrees.jpg","smile.jpg","try.jpg",]
-var htmlstring = '<div id ="gallery">\
-				 <img src = '+ picture[i] +' width=500px height=500px>\
-				 </div>';
-	$("#picture[i]").append(htmlstring);
+
+function galleryPics(){
+	for (var i = 0; i < picture.length; i++) { 
+		var picture = picture[i];
+		alert("hi");
+		var htmlstring = '<div><img src = "' +  picture[i] + '" width=500px height=500px></div>';
+		$("#gallery").append(htmlstring);
+	}
+};
+
 
