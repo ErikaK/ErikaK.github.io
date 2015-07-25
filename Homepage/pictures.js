c5a99774f449ae6ff60121d4788cc35818a370e4
neha45556 <neha45556@gmail.com>
2015-07-24 22:55:03 -0700

    games
    games

--- Homepage/pictures.js
+++ Homepage/pictures.js
@@ -1,10 +1,14 @@
-var picture = ["cal.jpg","flower.jpg","god.jpg","palmtrees.jpg","smile.jpg","try.jpg",]
+var picBox = ["cal.jpg","flower.jpg","god.jpg","palmtrees.jpg","smile.jpg","try.jpg",]
+
+//1)Make sure you use picBox wherever you use the list of photos
+//2) Decide what to call the variable holding one picture (that's at picBox[i]) and make sure your code has that variable name right.
 $(document).ready(galleryPics);
+
 function galleryPics(){
-	for (var i = 0; i < picture.length; i++) { 
-		var picture = picture[i];
+	for (var i = 0; i < picBox.length; i++) { 
+		var picture2 = pictureBox[i];
 		alert("hi");
-		var htmlstring = '<div><img src = "' +  picture[i] + '" width=500px height=500px></div>';
+		var htmlstring = '<div><img src = "' +  pictureBox[i] + '" width=500px height=500px></div>';
 		$("#gallery").append(htmlstring);
 	}
 };
